7ddeb3fbcc4811a985247af34eaf2f6eaedd391b
Michael Quarcoo <michaelquarcoo04@gmail.com>
2023-01-03 17:33:04 +0000

    CustomToolTip component added

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,26 +5,13 @@ import {
   CalendarMonthOutlined,
   PinDropOutlined,
 } from "@mui/icons-material";
-import { styled } from "@mui/material/styles";
-import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import CustomToolTip from "../CustomToolTip/CustomToolTip";
 import "./SearchBar.css";
 
 const SearchBar = () => {
   const search = (e) => {
     e.preventDefault();
   };
-
-  const CustomToolTip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-  ))(({ theme }) => ({
-    [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: "#f5f5f9",
-      color: "rgba(0, 0, 0, 0.87)",
-      maxWidth: 220,
-      fontSize: theme.typography.pxToRem(12),
-      border: "1px solid #dadde9",
-    },
-  }));
 
   return (
     <form id="searchBar" onSubmit={search}>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar/Navbar";
 import Header from "./Header/Header";
 import SearchBar from "./SearchBar/SearchBar";
 import EventTypes from "./EventTypes/EventTypes";
+import CustomToolTip from "./CustomToolTip/CustomToolTip";
 
-export { Navbar, Header, SearchBar, EventTypes };
+export { Navbar, Header, SearchBar, EventTypes, CustomToolTip };
